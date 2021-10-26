013b08b873c3958aa5cbc0b238614ad66e6575e6
shane <kxm874@student.bham.ac.uk>
2021-10-26 15:33:08 +0100

    updated countdown/testimonials/portfolio

--- motiiv/static/js/main.js
+++ motiiv/static/js/main.js
@@ -211,7 +211,7 @@
 
 })(jQuery);
 
-var countDate = new Date('Oct 21, 2021 00:00:00').getTime();
+var countDate = new Date('Jan 31, 2022 00:00:00').getTime();
 function countDown(){
   var now = new Date().getTime();
   gap = countDate -now;
